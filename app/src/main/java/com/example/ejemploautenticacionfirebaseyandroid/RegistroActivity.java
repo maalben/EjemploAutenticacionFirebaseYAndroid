9f57a6e51094ffd788d965330cdf17ace2c43f90
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2019-09-10 20:56:00 -0500

    Se modifica información para usar database de firebase

--- app/src/main/java/com/example/ejemploautenticacionfirebaseyandroid/RegistroActivity.java
+++ app/src/main/java/com/example/ejemploautenticacionfirebaseyandroid/RegistroActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +27,9 @@ public class RegistroActivity extends AppCompatActivity {
     private EditText usuario;
     private EditText clave;
     private EditText confirmarclave;
+    private EditText numerodocumento;
+    private EditText nombrecompleto;
+    private EditText edad;
     private Button guardar;
     private Button volver;
     private FirebaseAuth firebaseAuth;
@@ -36,6 +41,9 @@ public class RegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        numerodocumento = findViewById(R.id.txtdocumento);
+        nombrecompleto = findViewById(R.id.txtnombre);
+        edad = findViewById(R.id.txtedad);
         usuario = findViewById(R.id.txtusuario);
         clave = findViewById(R.id.txtclave);
         confirmarclave = findViewById(R.id.txtconfirmarclave);
@@ -45,19 +53,32 @@ public class RegistroActivity extends AppCompatActivity {
 
         inicializarFirebase();
 
+        numerodocumento.addTextChangedListener(validarcampo);
+        nombrecompleto.addTextChangedListener(validarcampo);
+        edad.addTextChangedListener(validarcampo);
+        usuario.addTextChangedListener(validarcampo);
+        clave.addTextChangedListener(validarcampo);
+        confirmarclave.addTextChangedListener(validarcampo);
+
+
         guardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String valorDocumento = numerodocumento.getText().toString().trim();
+                String valorNombre = nombrecompleto.getText().toString().trim();
+                String valorEdad = edad.getText().toString().trim();
                 String user = usuario.getText().toString().trim();
                 String pass = clave.getText().toString().trim();
                 String pass2 = confirmarclave.getText().toString().trim();
 
                 if(pass.equals(pass2)){
                     Usuarios usuarios = new Usuarios();
+                    usuarios.setNumerodocumento(valorDocumento);
+                    usuarios.setNombrecompleto(valorNombre);
+                    usuarios.setEdad(valorEdad);
                     usuarios.setEmail(user);
-                    usuarios.setClave(pass);
-
-                    firebaseAuth.createUserWithEmailAndPassword(usuarios.getEmail(), usuarios.getClave()).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+                    databaseReference.child("Usuarios").child(usuarios.getNumerodocumento()).setValue(usuarios);
+                    firebaseAuth.createUserWithEmailAndPassword(usuarios.getEmail(), pass).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(!task.isSuccessful()){
@@ -90,6 +111,44 @@ public class RegistroActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
     }
+
+    private TextWatcher validarcampo = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            //No is need
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            final String regex = "(?:[^<>()\\[\\].,;:\\s@\"]+(?:\\.[^<>()\\[\\].,;:\\s@\"]+)*|\"[^\\n\"]+\")@(?:[^<>()\\[\\].,;:\\s@\"]+\\.)+[^<>()\\[\\]\\.,;:\\s@\"]{2,63}";
+            String valorDocumento = numerodocumento.getText().toString().trim();
+            String valorNombre = nombrecompleto.getText().toString().trim();
+            String valorEdad = edad.getText().toString().trim();
+            String user = usuario.getText().toString().trim();
+            String pass = clave.getText().toString().trim();
+            String pass2 = confirmarclave.getText().toString().trim();
+
+            guardar.setEnabled(
+                    !valorDocumento.isEmpty() &&
+                    !valorNombre.isEmpty() &&
+                    !valorEdad.isEmpty() &&
+                    !user.isEmpty() &&
+                    !pass.isEmpty() &&
+                    !pass2.isEmpty() &&
+                    user.matches(regex) &&
+                    pass.length() > 5 &&
+                    pass2.length() > 5 &&
+                    (pass.equals(pass2)) &&
+                    valorDocumento.length() > 5
+            );
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            //No is need
+        }
+    };
 
 
 }
--- app/src/main/java/com/example/ejemploautenticacionfirebaseyandroid/model/Usuarios.java
+++ app/src/main/java/com/example/ejemploautenticacionfirebaseyandroid/model/Usuarios.java
@@ -4,6 +4,33 @@ public class Usuarios {
 
     private String email;
     private String clave;
+    private String numerodocumento;
+    private String nombrecompleto;
+    private String edad;
+
+    public String getNumerodocumento() {
+        return numerodocumento;
+    }
+
+    public void setNumerodocumento(String numerodocumento) {
+        this.numerodocumento = numerodocumento;
+    }
+
+    public String getNombrecompleto() {
+        return nombrecompleto;
+    }
+
+    public void setNombrecompleto(String nombrecompleto) {
+        this.nombrecompleto = nombrecompleto;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 
     public String getEmail() {
         return email;
